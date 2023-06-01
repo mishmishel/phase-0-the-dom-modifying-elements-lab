12cbd7039d516fdc72f2a60b316daba5e5fe458b
Mishel Tyurkova <misheltyurkova@gmail.com>
2023-06-02 08:20:34 +1000

    commit tree e2ef191443d57e2eb35507719253c9de64d06692

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // Write your code here!
 
-document.querySelector('#main').remove()
+let meow = document.querySelector('#main');
+meow.remove();
 
 const newHeader = document.createElement("H1"); 
 newHeader.setAttribute('id', 'victory');
 newHeader.innerHTML = 'Mishel is the champion';
 document.body.append(newHeader);
+
+
 
