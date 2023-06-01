b8940e09cbb2f8a5612799c8231e293ccd100eb4
Mishel Tyurkova <misheltyurkova@gmail.com>
2023-06-02 08:07:30 +1000

    Finished Lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+document.querySelector('#main').remove()
+
+const newHeader = document.createElement("H1"); 
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = 'Mishel is the champion';
+document.body.append(newHeader);
+
